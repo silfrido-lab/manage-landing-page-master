badacd1635490e45bd25cb514a60466d9ec4ec2b
Silfrido Benitez <silfrido123chiro@gmail.com>
2020-07-30 22:59:15 +0100

    mobile finish

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 // cuando cargue la ventana añadimos un evento notificador
 window.addEventListener('load', function() {
     new Glider(document.querySelector('.slider-list'), {
-        slidesToShow: 3,
+        // Mobile-first defaults
+        slidesToShow: 1,
         slidesToScroll: 1,
-        draggable: true,
+        scrollLock: true,
         dots: '.slider-indicator',
+        duration: 0.15,
 
+        responsive: [{
+            // screens greater than >= 775px
+            breakpoint: 775,
+            settings: {
+                // Set to `auto` and provide item width to adjust to viewport
+                slidesToShow: 'auto',
+                slidesToScroll: 'auto',
+                itemWidth: 150,
+                duration: 0.15
+            }
+        }, {
+            // screens greater than >= 1024px
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                itemWidth: 150,
+                duration: 0.15
+            }
+        }]
     });
-    console.log('iniciado');
 });
