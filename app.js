fea1f57cb8ba783469c37c1647c1ab562f1a4e61
Silfrido Benitez <silfrido123chiro@gmail.com>
2020-08-15 22:50:25 +0100

    footer grid desktop

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ window.addEventListener('load', function() {
             breakpoint: 775,
             settings: {
                 // Set to `auto` and provide item width to adjust to viewport
-                slidesToShow: 'auto',
-                slidesToScroll: 'auto',
+                slidesToShow: '2',
+                slidesToScroll: '1',
                 itemWidth: 150,
                 duration: 0.15
             }
@@ -22,7 +22,7 @@ window.addEventListener('load', function() {
             // screens greater than >= 1024px
             breakpoint: 1024,
             settings: {
-                slidesToShow: 2,
+                slidesToShow: 3,
                 slidesToScroll: 1,
                 itemWidth: 150,
                 duration: 1000
